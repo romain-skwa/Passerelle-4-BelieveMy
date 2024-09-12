236bac2056571946fdea9b6de32394897128e58a
Romain Delbos <nostromo_site@yahoo.fr>
2024-09-12 18:01:59 +0200

    la disposition est bonne

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import flagBG from "../../../public/flag/drapeau_uk.jpg";
 import flagFrance from "../../../public/flag/Flag_France.png";
 import homeIconWhite from "../../../public/logo/white-home-icon.png";
 import searchIconWhite from "../../../public/logo/Search-Button-White.png";
+import logowebsite from "../../../public/logo/double.jpg";
 
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
@@ -15,9 +16,9 @@ export default function Header() {
 
   return (
     <header>
-      <section className="flex justify-between lg:px-8 bg-black pt-3">
+      <section className="flex justify-between lg:px-8 bg-black pt-3 relative">
         {/* ----------------DRAPEAUX----------------------------------------------------------------- */}
-        <div className="flex w-1/3 h-10 ml-3 lg:ml-0 mt-2 ">
+        <div className="flex h-10 ml-3 lg:ml-0 mt-2 lg:w-40">
           <Image
             src={flagBG}
             alt="Drapeau Grande Bretagne"
@@ -30,31 +31,69 @@ export default function Header() {
           />
         </div>
 
-        {/* ----------------ICONES----------------------------------------------------------------- */}
+        {/* ----------------Logo------------------------- */}
+        <Image src={logowebsite} className="w-24 h-24" alt="Logo du site" />        
+ 
 
-        <div
-          className=" lg:w-1/3 h-8 flex justify-center relative" /* on ne met pas de flex parce que ça allonge la hauteur des logos */
-        >
-          <Image src={logoInstagram} alt="Logo instagram" className="w-8 h-8" />
+        {/* ----------------ICONES-------Réseaux sociaux------------------------------------------------------ */}
+        <div className="h-8 lg:flex mt-1">
+          <div className="flex mb-3">
+            <Image src={logoInstagram} alt="Logo instagram" className="w-7 h-7" />
+            <Image src={logoFacebook}  alt="Logo facebook"  className="w-7 h-7 mx-3" />
+          </div>
+          <div className="flex">
+            <Image src={logoTwitch} alt="Logo twitch" className="w-7 h-7 mr-3" />
+            <Image src={logoTwitch} alt="Logo twitch" className="w-7 h-7 mr-3" />
+          </div>
+        </div>
+      </section>
 
-          <Image
-            src={logoFacebook}
-            alt="Logo facebook"
-            className="w-8 h-8 mx-2"
-          />
-          <Image src={logoTwitch} alt="Logo twitch" className="w-8 h-8" />
-          {/*
-          <div className="lg:hidden text-center w-full  bg-orange-400 absolute bottom-0">
+      {/* ----------------LIGNE 2------------------------------------------------------------- */}
+      {/* ------------------------------------------------------------------------------------ */}
+
+      <section className="flex flex-col lg:flex-row px-2 lg:px-8 pt-2 items-center lg:justify-between relative bg-gradient-to-b from-black to-dark-french-blue">
+        <div className="flex" /* Part left */>
+          {/* ----------------Accueil------------------------- */}
+          <div className="text-center hidden lg:block lg:mr-4">
             <Link href="../../">Accueil</Link>
           </div>
-          */}
+          {/* ----------------Recherche------------------------- */}
+          <div className="text-center m-2 lg:m-0 hidden lg:block ">
+            Recherche
+          </div>
         </div>
+
+
+        {/* ----------------Formulaire Présentation------------------------- */}
+        <div className="order-last lg:order-none"  /* Part Middle */>
+          <div className={`flex flex-col lg:flex-row pb-3 lg:pb-0`}>
+            {session?.user?.email ? (
+              <div className="cursor-pointer text-center pl-3 pb-2 lg:pb-0 lg:p-2 order-last lg:order-none">
+                <Link href="../../creators/formularyintroduction">
+                  Formulaire de présentation de votre jeu
+                </Link>
+              </div>
+            ) : (
+              <div></div>
+            )}
+
+            {/* ----------------Comment présenter------------------------- */}
+            <div
+              className={`text-center p-2 ml-3 ${
+                !session?.user?.email ? "flex-grow" : ""
+              }`}
+            >
+              Comment présenter votre jeu ?
+            </div>
+          </div>
+        </div>
+
 
         {/* -----------CONNEXION-----INSCRIPTION-------------------------------------------------------------- */}
 
-        <div className=" w-1/3  text-sm lg:text-base h-10 uppercase flex flex-col lg:flex-row lg:justify-end lg:pt-1">
+        <div className="text-sm lg:text-base h-10 uppercase flex lg:justify-end lg:pt-1">
           <div
-            className="mr-2 lg:mr-4 flex justify-end" /* Petite marge externe ajoutée quand on est sur un écran pc. Le margin créé un espace vide entre les div. un padding aurait agrandit la zone cliquable*/
+            className="mr-2 lg:mr-4 flex justify-end"  /* Part right */
           >
             {session?.user?.email ? (
               <div className="cursor-pointer" onClick={() => signOut()}>
@@ -67,54 +106,22 @@ export default function Header() {
           <div className="pl-3 mr-2 flex justify-end">
             <Link href="../../creators/register">S'inscrire</Link>
           </div>
-        </div>
-      </section>
-
-      {/* ----------------LIGNE 2------------------------------------------------------------- */}
-
-      <section className="flex flex-col lg:flex-row px-2 lg:px-8 pt-2 items-center relative bg-gradient-to-b from-black to-dark-french-blue">
+                  {/* --------Icone Home visible sur écran mobiles------------ */}
         <Link href="../../">
           <Image
             src={homeIconWhite}
             alt="Home Icon"
-            className="w-7 h-7 opacity-70 absolute top-1 left-0 ml-4 mt-2 lg:hidden"
+            className="w-7 h-7 opacity-70 absolute left-0 ml-4 top-1 lg:hidden"
           />
         </Link>
-
-        <div className="border text-center lg:text-left hidden lg:block lg:w-2/5">
-          <Link href="../../">Accueil</Link>
-        </div>
-
-        <div className="text-center m-2 lg:m-0 bg-orange-400 hidden lg:block flex-grow w-full">
-          <input type="text" placeholder="Recherche" />
-        </div>
-
-       <div className={`flex flex-col lg:flex-row lg:w-2/5 ${session?.user?.email ? "lg:w-3/5" : ""}`}>
-
-          {session?.user?.email ? (
-            <div className="cursor-pointer text-center pl-3 pb-2 lg:pb-0 lg:p-2 order-last lg:order-none">
-              <Link href="../../creators/formularyintroduction">
-                Formulaire de présentation de votre jeu
-              </Link>
-            </div>
-          ) : (
-            <div></div>
-          )} 
-
-          <div className={`text-center p-2 ml-3 ${!session?.user?.email ? "flex-grow" : ""}`}>
-            Comment présenter votre jeu ?
-          </div>
-
-          <Image
-            src={searchIconWhite}
-            alt="Search Icon"
-            className="w-8 h-8 absolute top-1 right-0 mr-4 mt-2 lg:hidden"
-          />
-
-
+                  {/* --------Icone Search visible sur écran mobiles------------ */}
+        <Image
+          src={searchIconWhite}
+          alt="Search Icon"
+          className="w-8 h-8 absolute right-0 mr-4 top-1 lg:hidden"
+        />
         </div>
       </section>
     </header>
   );
 }
-//        <div className="w-28 h-24 bg-red-900 ">LOGO</div>
